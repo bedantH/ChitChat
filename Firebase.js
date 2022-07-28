a7193b3c28af3237ae043433f7b42d6238936f4e
Bedant Hota <64542454+bedantH@users.noreply.github.com>
2022-07-28 13:32:38 +0530

    removed sesitive api keys

--- Firebase.js
+++ Firebase.js
@@ -3,14 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCy8DmPe2NYtzVTJyzgsyy-XJqOzQHIrsc",
-    authDomain: "chitchat-44c18.firebaseapp.com",
-    databaseURL: "https://chitchat-44c18-default-rtdb.firebaseio.com",
-    projectId: "chitchat-44c18",
-    storageBucket: "chitchat-44c18.appspot.com",
-    messagingSenderId: "917587393253",
-    appId: "1:917587393253:web:761720990cba4773cfb20c",
-    measurementId: "G-ZXK8XBJBFT"
+    // firebase config goes here...
 };
 
 const app = firebase.initializeApp(firebaseConfig);
